3d58146efe7fa9c818da0095e64eb85a5c6aa378
densl <zengpush@gmail.com>
2018-02-26 15:18:01 +0800

    Fix widget view no response and remove inner service

--- app/src/main/java/net/antoniy/gidder/beta/ui/widget/ToggleAppWidgetProvider.java
+++ app/src/main/java/net/antoniy/gidder/beta/ui/widget/ToggleAppWidgetProvider.java
@@ -1,73 +1,41 @@
 package net.antoniy.gidder.beta.ui.widget;
 
-import net.antoniy.gidder.beta.R;
-import net.antoniy.gidder.beta.ui.util.C;
-import net.antoniy.gidder.beta.ui.util.GidderCommons;
 import android.app.PendingIntent;
-import android.app.Service;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.Configuration;
-import android.os.IBinder;
 import android.widget.RemoteViews;
-import android.widget.RemoteViews.RemoteView;
+
+import net.antoniy.gidder.beta.R;
+import net.antoniy.gidder.beta.ui.util.C;
+import net.antoniy.gidder.beta.ui.util.GidderCommons;
 
 public class ToggleAppWidgetProvider extends AppWidgetProvider {
 
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-		context.startService(new Intent(context, MyUpdateService.class));
-	}
-	
-	public static class MyUpdateService extends Service {
-		
-		@Override
-		public int onStartCommand(Intent intent, int flags, int startId) {
-			// Update the widget
-			buildRemoteView(this);
 
-			// No more updates so stop the service and free resources
-			stopSelf();
-			
-			return Service.START_NOT_STICKY;
-		}
-		
-		public void buildRemoteView(Context context) {
-			RemoteViews updateView = null;
-
-			PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, new Intent(C.action.TOGGLE_SSH_SERVER), PendingIntent.FLAG_UPDATE_CURRENT);
-			
-			RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.toggle_widget);
-			views.setOnClickPendingIntent(R.id.toggleWidgetButton, pendingIntent);
-			
-			if(GidderCommons.isSshServiceRunning(context)) {
-				views.setImageViewResource(R.id.toggleWidgetButton, R.drawable.ic_widget_active);
-			} else {
-				views.setImageViewResource(R.id.toggleWidgetButton, R.drawable.ic_widget_inactive);
-			}
-
-			ComponentName myWidget = new ComponentName(this, ToggleAppWidgetProvider.class);
-			AppWidgetManager manager = AppWidgetManager.getInstance(this);
-			manager.updateAppWidget(myWidget, updateView);
-		}
-
-		@Override
-		public void onConfigurationChanged(Configuration newConfig) {
-			int oldOrientation = this.getResources().getConfiguration().orientation;
-
-			if (newConfig.orientation != oldOrientation) {
-				// Update the widget
-				buildRemoteView(this);
-			}
-		}
-
-		@Override
-		public IBinder onBind(Intent intent) {
-			return null;
+		for (int appWidgetId : appWidgetIds) {
+			updateAppWidget(context, appWidgetManager, appWidgetId);
 		}
 	}
 
+	public void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+
+		PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, new Intent(C.action.TOGGLE_SSH_SERVER), PendingIntent.FLAG_UPDATE_CURRENT);
+
+		RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.toggle_widget);
+		remoteViews.setOnClickPendingIntent(R.id.toggleWidgetButton, pendingIntent);
+
+		if (GidderCommons.isSshServiceRunning(context)) {
+			remoteViews.setImageViewResource(R.id.toggleWidgetButton, R.drawable.ic_widget_active);
+		} else {
+			remoteViews.setImageViewResource(R.id.toggleWidgetButton, R.drawable.ic_widget_inactive);
+		}
+
+		ComponentName componentName = new ComponentName(context, ToggleAppWidgetProvider.class);
+		appWidgetManager.updateAppWidget(componentName, remoteViews);
+	}
 }
